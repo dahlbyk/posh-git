e11ab0d45a99abb9f6e13aeb65520674f5fd20dd
dahlbyk <dahlbyk@gmail.com>
2010-04-02 03:20:27 -0500

    Import-Module comments in example profile

--- profile.example.ps1
+++ profile.example.ps1
@@ -1,6 +1,12 @@
 Push-Location (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
+# Load posh-git module from current directory
 Import-Module .\posh-git
+
+# If module is installed in a default location ($env:PSModulePath),
+# use this instead (see about_Modules for more information):
+# Import-Module posh-git
+
 
 # Set up a simple prompt, adding the git prompt parts inside git repos
 function prompt {
