5aa17a5a8f8bfb33eedc141619e5eb68164468bd
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2010-04-05 21:16:15 +0100

    Sample profile (ported from posh-git)

---
+++ profile.example.ps1
@@ -0,0 +1,40 @@
+Push-Location (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
+
+# Load posh-hg module from current directory
+Import-Module .\posh-hg
+
+# If module is installed in a default location ($env:PSModulePath),
+# use this instead (see about_Modules for more information):
+# Import-Module posh-hg
+
+
+# Set up a simple prompt, adding the hg prompt parts inside hg repos
+function prompt {
+    Write-Host($pwd) -nonewline
+        
+    # Mercurial Prompt
+    $Global:HgStatus = Get-HgStatus
+    Write-HgStatus $HgStatus
+      
+    return "> "
+}
+
+if(-not (Test-Path Function:\DefaultTabExpansion)) {
+    Rename-Item Function:\TabExpansion DefaultTabExpansion
+}
+
+# Set up tab expansion and include hg expansion
+function TabExpansion($line, $lastWord) {
+    $lastBlock = [regex]::Split($line, '[|;]')[-1]
+    
+    switch -regex ($lastBlock) {
+        # mercurial and tortoisehg tab expansion
+        'hg (.*)' { HgTabExpansion($lastBlock) }
+        'hgtk (.*)' { HgtkTabExpansion($lastBlock) }
+        # Fall back on existing tab expansion
+        default { DefaultTabExpansion $line $lastWord }
+    }
+}
+
+
+Pop-Location
