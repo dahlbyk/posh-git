9b44e12e513e9fa4e97b2c1b9d8598da964cdc07
Keith Dahlby <dahlbyk@gmail.com>
2010-12-03 22:56:47 -0600

    Install script cleanup

--- install.ps1
+++ install.ps1
@@ -1,22 +1,19 @@
 param([switch]$WhatIf = $false)
 
-if(!(Get-Command git -ErrorAction SilentlyContinue))
-{
+if(!(Get-Command git -ErrorAction SilentlyContinue)) {
     Write-Warning 'Could not find git command. Please create a git alias or add %ProgramFiles%\Git\cmd to PATH.'
 }
 
-if(!(Test-Path $PROFILE))
-{
+if(!(Test-Path $PROFILE)) {
     Write-Host "Creating PowerShell profile...`n$PROFILE"
     New-Item $PROFILE -Force -Type File -ErrorAction Stop -WhatIf:$WhatIf > $null
 }
 
 $installDir = Split-Path $MyInvocation.MyCommand.Path -Parent
 $profileLine = ". $installDir\profile.example.ps1"
-if(!(Select-String -Path $PROFILE -Pattern $profileLine -Quiet -SimpleMatch))
-{
+if(!(Select-String -Path $PROFILE -Pattern $profileLine -Quiet -SimpleMatch)) {
     Write-Host "Adding posh-git to profile..."
-    "`n# Load posh-git sample profile`n$profileLine`n`n" | Out-File $PROFILE -Append -WhatIf:$WhatIf
+    "`n`n# Load posh-git example profile`n$profileLine`n" | Out-File $PROFILE -Append -WhatIf:$WhatIf
 }
 
 Write-Host 'posh-git sucessfully installed!'
