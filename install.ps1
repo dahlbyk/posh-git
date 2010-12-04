3449b40ce347a564347d278f2739c022eb4cbc06
Keith Dahlby <dahlbyk@gmail.com>
2010-12-03 23:05:32 -0600

    Update install script to respect existing profile's encoding

--- install.ps1
+++ install.ps1
@@ -9,11 +9,27 @@ if(!(Test-Path $PROFILE)) {
     New-Item $PROFILE -Force -Type File -ErrorAction Stop -WhatIf:$WhatIf > $null
 }
 
+# Adapted from http://www.west-wind.com/Weblog/posts/197245.aspx
+function Get-FileEncoding($Path) {
+    $bytes = [byte[]](Get-Content $Path -Encoding byte -ReadCount 4 -TotalCount 4)
+
+    if(!$bytes) { return 'utf8' }
+
+    switch -regex ('{0:x2}{1:x2}{2:x2}{3:x2}' -f $bytes[0],$bytes[1],$bytes[2],$bytes[3]) {
+        '^efbbbf'   { return 'utf8' }
+        '^2b2f76'   { return 'utf7' }
+        '^fffe'     { return 'unicode' }
+        '^feff'     { return 'bigendianunicode' }
+        '^0000feff' { return 'utf32' }
+        default     { return 'ascii' }
+    }
+}
+
 $installDir = Split-Path $MyInvocation.MyCommand.Path -Parent
 $profileLine = ". $installDir\profile.example.ps1"
 if(!(Select-String -Path $PROFILE -Pattern $profileLine -Quiet -SimpleMatch)) {
     Write-Host "Adding posh-git to profile..."
-    "`n`n# Load posh-git example profile`n$profileLine`n" | Out-File $PROFILE -Append -WhatIf:$WhatIf
+    "`n`n# Load posh-git example profile`n$profileLine`n" | Out-File $PROFILE -Append -WhatIf:$WhatIf -Encoding (Get-FileEncoding $PROFILE)
 }
 
 Write-Host 'posh-git sucessfully installed!'
