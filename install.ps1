b749f6b40b114a48533b74420ae51255d6242861
Keith Dahlby <dahlbyk@gmail.com>
2010-12-07 12:46:21 -0600

    Update install.ps1 to quit early on error

--- install.ps1
+++ install.ps1
@@ -2,6 +2,7 @@ param([switch]$WhatIf = $false)
 
 if(!(Get-Command git -ErrorAction SilentlyContinue)) {
     Write-Warning 'Could not find git command. Please create a git alias or add %ProgramFiles%\Git\cmd to PATH.'
+    return
 }
 
 if(!(Test-Path $PROFILE)) {
@@ -27,10 +28,13 @@ function Get-FileEncoding($Path) {
 
 $installDir = Split-Path $MyInvocation.MyCommand.Path -Parent
 $profileLine = ". $installDir\profile.example.ps1"
-if(!(Select-String -Path $PROFILE -Pattern $profileLine -Quiet -SimpleMatch)) {
-    Write-Host "Adding posh-git to profile..."
-    "`n`n# Load posh-git example profile`n$profileLine`n" | Out-File $PROFILE -Append -WhatIf:$WhatIf -Encoding (Get-FileEncoding $PROFILE)
+if(Select-String -Path $PROFILE -Pattern $profileLine -Quiet -SimpleMatch) {
+    Write-Host "It seems posh-git is already installed..."
+    return
 }
+
+Write-Host "Adding posh-git to profile..."
+"`n`n# Load posh-git example profile`n$profileLine`n" | Out-File $PROFILE -Append -WhatIf:$WhatIf -Encoding (Get-FileEncoding $PROFILE)
 
 Write-Host 'posh-git sucessfully installed!'
 Write-Host 'Please reload your profile for the changes to take effect:'
