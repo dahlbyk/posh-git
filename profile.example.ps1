841af46e2cc56c627e07214b284b3a62997c1d4f
Keith Dahlby <dahlbyk@gmail.com>
2011-09-18 22:54:38 -0500

    Disable tab expansion if Function:\TabExpansion is not found (PowerShell 3)

--- profile.example.ps1
+++ profile.example.ps1
@@ -25,19 +25,21 @@ function prompt {
     return "> "
 }
 
-$teBackup = 'posh-git_DefaultTabExpansion'
-if(!(Test-Path Function:\$teBackup)) {
-    Rename-Item Function:\TabExpansion $teBackup
-}
+if(Test-Path Function:\TabExpansion) {
+    $teBackup = 'posh-git_DefaultTabExpansion'
+    if(!(Test-Path Function:\$teBackup)) {
+        Rename-Item Function:\TabExpansion $teBackup
+    }
 
-# Set up tab expansion and include git expansion
-function TabExpansion($line, $lastWord) {
-    $lastBlock = [regex]::Split($line, '[|;]')[-1].TrimStart()
-    switch -regex ($lastBlock) {
-        # Execute git tab completion for all git-related commands
-        "$(Get-GitAliasPattern) (.*)" { GitTabExpansion $lastBlock }
-        # Fall back on existing tab expansion
-        default { & $teBackup $line $lastWord }
+    # Set up tab expansion and include git expansion
+    function TabExpansion($line, $lastWord) {
+        $lastBlock = [regex]::Split($line, '[|;]')[-1].TrimStart()
+        switch -regex ($lastBlock) {
+            # Execute git tab completion for all git-related commands
+            "$(Get-GitAliasPattern) (.*)" { GitTabExpansion $lastBlock }
+            # Fall back on existing tab expansion
+            default { & $teBackup $line $lastWord }
+        }
     }
 }
 
