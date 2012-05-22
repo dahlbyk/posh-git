253e6a4cb47d7a9611678fdc187ebe74a098535e
LokiMidgard <loki.midgard@live.de>
2012-05-22 16:44:01 +0200

    Use old prompt if no git repo
    Using old prompt as backup if current location is not part of a git
    reposetory. This makes this script more compatible to other modules that
    change the Prompt.

--- profile.example.ps1
+++ profile.example.ps1
@@ -7,20 +7,28 @@ Import-Module .\posh-git
 # use this instead (see about_Modules for more information):
 # Import-Module posh-git
 
+# Rename the old prompt function for later access
+Rename-Item function:prompt _PoshGitAliasPrompt
 
 # Set up a simple prompt, adding the git prompt parts inside git repos
 function prompt {
     $realLASTEXITCODE = $LASTEXITCODE
 
-    # Reset color, which can be messed up by Enable-GitColors
-    $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
+    if(Get-GitDirectory) { # check if this is a git repo
+    
+        # Reset color, which can be messed up by Enable-GitColors
+        $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
 
-    Write-Host($pwd) -nonewline
+        Write-Host($pwd) -nonewline
 
-    Write-VcsStatus
+        Write-VcsStatus
 
-    $global:LASTEXITCODE = $realLASTEXITCODE
-    return "> "
+        $global:LASTEXITCODE = $realLASTEXITCODE
+        return "> "
+    } else { # if not execute old prompt
+        $global:LASTEXITCODE = $realLASTEXITCODE
+        return _PoshGitAliasPrompt
+    }
 }
 
 Enable-GitColors
