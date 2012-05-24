3f9a282d22b3323fe25516697aef879174c3b62e
eamodio <eamodio@gmail.com>
2012-05-24 15:20:16 -0400

    Added file tab expansion for diff/difftool and merge/mergetool
    Closes #48

--- GitTabExpansion.ps1
+++ GitTabExpansion.ps1
@@ -101,6 +101,18 @@ function script:gitCheckoutFiles($filter) {
     gitFiles $filter (@($GitStatus.Working.Unmerged) + @($GitStatus.Working.Modified) + @($GitStatus.Working.Deleted))
 }
 
+function script:gitDiffFiles($filter, $staged) {
+    if ($staged) {
+        gitFiles $filter $GitStatus.Index.Modified
+    } else {
+        gitFiles $filter (@($GitStatus.Working.Unmerged) + @($GitStatus.Working.Modified) + @($GitStatus.Index.Modified))
+    }
+}
+
+function script:gitMergeFiles($filter) {
+    gitFiles $filter $GitStatus.Working.Unmerged
+}
+
 function script:gitDeleted($filter) {
     gitFiles $filter $GitStatus.Working.Deleted
 }
@@ -222,6 +234,16 @@ function GitTabExpansion($lastBlock) {
         # Handles git rm <path>
         "^rm.* (?<index>\S*)$" {
             gitDeleted $matches['index']
+        }
+
+        # Handles git diff/difftool <path>
+        "^(?:diff|difftool)(?:.* (?<staged>(?:--cached|--staged))|.*) (?<files>\S*)$" {
+            gitDiffFiles $matches['files'] $matches['staged']
+        }
+
+        # Handles git merge/mergetool <path>
+        "^(?:merge|mergetool).* (?<files>\S*)$" {
+            gitMergeFiles $matches['files']
         }
 
         # Handles git <cmd> <ref>
