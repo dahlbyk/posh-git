d98c69a8519176b37620ab4ee78e4d61ba080712
Matt Wrock <matt@mattwrock.com>
2012-05-25 01:39:10 -0700

    gracefully update posh-git

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -1,3 +1,7 @@
+function Insert-Script([ref]$originalScript, $script) {
+    if(!($originalScript.Value -Contains $script)) { $originalScript.Value += $script }
+}
+
 try {
     $binRoot = join-path $env:systemdrive 'tools'
 
@@ -14,8 +18,26 @@ try {
       Write-Host 'Could not remove poshgit folder'
     }
 
+    $poshGitInstall = if($env:poshGit -ne $null){ $env:poshGit } else {'https://github.com/dahlbyk/posh-git/zipball/master'}
     #Install-ChocolateyZipPackage 'poshgit' 'https://github.com/dahlbyk/posh-git/zipball/v0.4' $poshgitPath
-    Install-ChocolateyZipPackage 'poshgit' 'https://github.com/dahlbyk/posh-git/zipball/master' $poshgitPath
+    Install-ChocolateyZipPackage 'poshgit' $poshGitInstall $poshgitPath
+    $pgitDir = [Array](Dir "$poshgitPath\*posh-git*\" | Sort-Object -Property LastWriteTime)[-1]
+
+    if(Test-Path $PROFILE) {
+        $oldProfile = [string[]](Get-Content $PROFILE)
+        $newProfile = @()
+        $lib = "$env:chocolateyInstall\lib"
+        #Clean out old profiles
+        $pgitExample = "$pgitDir\profile.example.ps1"
+        foreach($line in $oldProfile) {
+            if($line.ToLower().Contains("$poshgitPath".ToLower())) { $line = ". '$pgitExample'" }
+            if($line.Trim().Length -gt 0) {  $newProfile += $line }
+        }
+        #Save any previous Prompt logic
+        Insert-Script ([REF]$newProfile) "if(Test-Path Function:\Prompt) {Rename-Item Function:\Prompt PrePoshGitPrompt -Force}"
+        Set-Content -path $profile -value $newProfile -Force
+    }
+
 
     #------- ADDITIONAL SETUP -------#
     $subfolder = get-childitem $poshgitPath -recurse -include 'dahlbyk-posh-git-*' | select -First 1
@@ -24,8 +46,19 @@ try {
     $installer = Join-Path $subfolder 'install.ps1'
     & $installer
 
+    if($oldProfile) {
+        $newProfile = [string[]](Get-Content $PROFILE)
+        Insert-Script ([REF]$newProfile) "Rename-Item Function:\Prompt PoshGitPrompt -Force"
+        Insert-Script ([REF]$newProfile) "function Prompt() {if(Test-Path Function:\PrePoshGitPrompt){PrePoshGitPrompt}PoshGitPrompt}"
+        Set-Content -path $profile  -value $newProfile -Force
+    }
+
     Write-ChocolateySuccess 'poshgit'
 } catch {
+  try {
+    if($oldProfile){ Set-Content -path $PROFILE -value $oldProfile -Force }
+  }
+  catch{}  
   Write-ChocolateyFailure 'poshgit' $($_.Exception.Message)
   throw
 }
