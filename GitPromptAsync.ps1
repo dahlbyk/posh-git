9563378c5eb7d19de2ed6e559f6318fd9efa7aa4
Robert Ream <robertream@gmail.com>
2012-06-15 14:17:19 -0700

    updates to a repository if the current location is not in the repository were using the current location to update the status, which corrupted the status

--- GitPromptAsync.ps1
+++ GitPromptAsync.ps1
@@ -1,24 +1,24 @@
 function Start-GitPrompt([switch] $Asynchronous) {
     if (-not $GitPromptState) {
-		if ($Asynchronous) {
-			$timer = New-Object Timers.Timer -Property @{
-				Interval = 250
-				AutoReset = $true
-				Enabled = $true
-			}
-			Set-Variable GitPromptState -Scope Global -Value (New-Object PSObject -Property @{
-				Asynchronous = $true
-				Repositories = @{ }
-				Timer = $timer
-				Elapsed = Register-ObjectEvent $timer Elapsed -Action { Update-GitPromptRepositories }
-			})
-		}
-		else {
-			Set-Variable GitPromptState -Scope Global -Value (New-Object PSObject -Property @{
-				Asynchronous = $false
-				Repositories = @{ }
-			})
-		}
+        if ($Asynchronous) {
+            $timer = New-Object Timers.Timer -Property @{
+                Interval = 250
+                AutoReset = $true
+                Enabled = $true
+            }
+            Set-Variable GitPromptState -Scope Global -Value (New-Object PSObject -Property @{
+                Asynchronous = $true
+                Repositories = @{ }
+                Timer = $timer
+                Elapsed = Register-ObjectEvent $timer Elapsed -Action { Update-GitPromptRepositories }
+            })
+        }
+        else {
+            Set-Variable GitPromptState -Scope Global -Value (New-Object PSObject -Property @{
+                Asynchronous = $false
+                Repositories = @{ }
+            })
+        }
     }
 }
 
@@ -29,7 +29,9 @@ function Update-GitPromptRepositories {
             $repository = $repositories[$key]
             if ($repository.LastStatusUpdate -lt $repository.LastUpdate) {
                 if ($repository.LastStatusUpdate.AddMilliseconds($repository.StatusUpdateIntervalBuffer) -lt [DateTime]::Now) {
-                    $repository.Status = (Get-GitStatus $repository.Path)
+                    Push-Location $Path
+                    try { $repository.Status = Get-GitStatus $repository.Path }
+                    finally { Pop-Location }
                 }
                 $repository.LastStatusUpdate = [DateTime]::Now
             }
@@ -39,27 +41,27 @@ function Update-GitPromptRepositories {
 
 function Stop-GitPrompt {
     if ($GitPromptState) {
-		if ($GitPromptState.Asynchronous) {
-			$GitPromptState.Timer.Enabled = $false
-			Unregister-Event $GitPromptState.Elapsed.Id
-		}
+        if ($GitPromptState.Asynchronous) {
+            $GitPromptState.Timer.Enabled = $false
+            Unregister-Event $GitPromptState.Elapsed.Id
+        }
         $GitPromptState.Repositories.Keys | ForEach-Object { Stop-GitPromptRepository $_ }
         Remove-Variable GitPromptState -Scope Global
     }
 }
 
-function Start-GitPromptRepository($path) {
-    if ($GitPromptState -and (-not $GitPromptState.Repositories.ContainsKey($path))) {
+function Start-GitPromptRepository($Path) {
+    if ($GitPromptState -and (-not $GitPromptState.Repositories.ContainsKey($Path))) {
         $repositories = $GitPromptState.Repositories.Clone()
-        $watcher = New-Object IO.FileSystemWatcher $path, '*.*' -Property @{
+        $watcher = New-Object IO.FileSystemWatcher $Path, '*.*' -Property @{
             IncludeSubdirectories = $true
             EnableRaisingEvents = $true
         }
-        $watcherAction = [scriptblock]::Create( { Publish-GitPromptRepositoryUpdated $path $eventArgs.Name }.ToString().Replace('$path', $path) )
+        $watcherAction = [scriptblock]::Create( { Publish-GitPromptRepositoryUpdated $Path $eventArgs.Name }.ToString().Replace('$Path', $Path) )
 
-        $repositories[$path] = New-Object PSObject -Property @{
-            Path = $path
-            Status = (Get-GitStatus $path)
+        $repositories[$Path] = New-Object PSObject -Property @{
+            Path = $Path
+            Status = (Get-GitStatus $Path)
             StatusUpdateIntervalBuffer = 500
             LastStatusUpdate = ([DateTime]::Now)
             LastUpdate = ([DateTime]::Now)
@@ -74,17 +76,17 @@ function Start-GitPromptRepository($path) {
     }
 }
 
-function Publish-GitPromptRepositoryUpdated($path, $file) {
-    if ($GitPromptState -and $GitPromptState.Repositories.ContainsKey($path)) {
-        $repository = $GitPromptState.Repositories.Get_Item($path)
-        if ($repository.Ignore | Where-Object { $file -match $_ }) { return }
+function Publish-GitPromptRepositoryUpdated($Path, $File) {
+    if ($GitPromptState -and $GitPromptState.Repositories.ContainsKey($Path)) {
+        $repository = $GitPromptState.Repositories.Get_Item($Path)
+        if ($repository.Ignore | Where-Object { $File -match $_ }) { return }
         $repository.LastUpdate = [DateTime]::Now
     }
 }
 
-function Stop-GitPromptRepository($path) {
-    if ($GitPromptState -and $GitPromptState.Repositories.ContainsKey($path)) {
-        $repository = $GitPromptState.Repositories[$path]
+function Stop-GitPromptRepository($Path) {
+    if ($GitPromptState -and $GitPromptState.Repositories.ContainsKey($Path)) {
+        $repository = $GitPromptState.Repositories[$Path]
         $repository.Watcher.EnableRaisingEvents = $false
         Unregister-Event $repository.Changed.Id
         Unregister-Event $repository.Created.Id
@@ -92,36 +94,38 @@ function Stop-GitPromptRepository($path) {
         Unregister-Event $repository.Renamed.Id
 
         $repositories = $GitPromptState.Repositories.Clone()
-        $repositories.Remove($path)
+        $repositories.Remove($Path)
         $GitPromptState.Repositories = $repositories
     }
 }
 
-function Update-GitPromptRepository($path) {
-    if ($GitPromptState -and $GitPromptState.Repositories.ContainsKey($path) -and (-not $GitPromptState.Asynchronous)) {
-		$repository = $GitPromptState.Repositories[$path]
-		if ($repository.LastStatusUpdate -lt $repository.LastUpdate) {
-			$repository.Status = Get-GitStatus $repository.Path
-			$repository.LastStatusUpdate = [DateTime]::Now
-		}
-	}
+function Update-GitPromptRepository($Path) {
+    if ($GitPromptState -and $GitPromptState.Repositories.ContainsKey($Path) -and (-not $GitPromptState.Asynchronous)) {
+        $repository = $GitPromptState.Repositories[$Path]
+        if ($repository.LastStatusUpdate -lt $repository.LastUpdate) {
+            Push-Location $Path
+            try { $repository.Status = Get-GitStatus $repository.Path }
+            finally { Pop-Location }
+            $repository.LastStatusUpdate = [DateTime]::Now
+        }
+    }
 }
 
 function Get-GitPromptStatus {
-    $path = Get-GitDirectory
-    if ($path) {
-		$repositoryPath = Split-Path -parent $path
+    $Path = Get-GitDirectory
+    if ($Path) {
+        $repositoryPath = Split-Path -parent $Path
         if ($GitPromptState) {
-			if ($GitPromptState.Repositories.ContainsKey($repositoryPath)) {
-				Update-GitPromptRepository $repositoryPath
-			}
-			else {
-				Start-GitPromptRepository $repositoryPath
-			}
+            if ($GitPromptState.Repositories.ContainsKey($repositoryPath)) {
+                Update-GitPromptRepository $repositoryPath
+            }
+            else {
+                Start-GitPromptRepository $repositoryPath
+            }
             $GitPromptState.Repositories[$repositoryPath].Status
         }
         else {
-            Get-GitStatus $path
+            Get-GitStatus $Path
         }
     }
 }
