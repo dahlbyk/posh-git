d7e4b3eebb24e6ff92902c7b789c699f6f9b8255
Rob Reynolds <ferventcoder@gmail.com>
2012-07-17 14:31:34 -0500

    updating poshgit

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -4,12 +4,24 @@ try {
     ### Using an environment variable to to define the bin root until we implement YAML configuration ###
     if($env:chocolatey_bin_root -ne $null){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
     $poshgitPath = join-path $binRoot 'poshgit'
+    
+    try {
+      if (test-path($poshgitPath)) {
+        Write-Host "Attempting to remove existing `'$poshgitPath`' prior to install."
+        remove-item $poshgitPath -recurse -force
+      }
+    } catch {
+      Write-Host 'Could not remove poshgit folder'
+    }
 
-    Install-ChocolateyZipPackage 'poshgit' 'https://github.com/dahlbyk/posh-git/zipball/v0.4' $poshgitPath
+    #Install-ChocolateyZipPackage 'poshgit' 'https://github.com/dahlbyk/posh-git/zipball/v0.4' $poshgitPath
+    Install-ChocolateyZipPackage 'poshgit' 'https://github.com/dahlbyk/posh-git/zipball/master' $poshgitPath
 
     #------- ADDITIONAL SETUP -------#
-    $installer = Join-Path $poshgitPath 'dahlbyk-posh-git-60be436'
-    $installer = Join-Path $installer 'install.ps1'
+    $subfolder = get-childitem $poshgitPath -recurse -include 'dahlbyk-posh-git-*' | select -First 1
+    write-debug "Found and using folder `'$subfolder`'"
+    #$installer = Join-Path $poshgitPath $subfolder #'dahlbyk-posh-git-60be436'
+    $installer = Join-Path $subfolder 'install.ps1'
     & $installer
 
     Write-ChocolateySuccess 'poshgit'
