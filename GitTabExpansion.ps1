c75a75150ed0b8036955728b35a632ca923b3bea
Keith Dahlby <dahlbyk@gmail.com>
2012-08-07 22:54:32 -0500

    Use git branch --no-color

--- GitTabExpansion.ps1
+++ GitTabExpansion.ps1
@@ -51,8 +51,8 @@ function script:gitBranches($filter, $includeHEAD = $false) {
         $prefix = $matches['from']
         $filter = $matches['to']
     }
-    $branches = @(git -c color.branch=false branch | foreach { if($_ -match "^\*?\s*(?<ref>.*)") { $matches['ref'] } }) +
-                @(git -c color.branch=false branch -r | foreach { if($_ -match "^  (?<ref>\S+)(?: -> .+)?") { $matches['ref'] } }) +
+    $branches = @(git branch --no-color | foreach { if($_ -match "^\*?\s*(?<ref>.*)") { $matches['ref'] } }) +
+                @(git branch --no-color -r | foreach { if($_ -match "^  (?<ref>\S+)(?: -> .+)?") { $matches['ref'] } }) +
                 @(if ($includeHEAD) { 'HEAD','FETCH_HEAD','ORIG_HEAD','MERGE_HEAD' })
     $branches |
         where { $_ -ne '(no branch)' -and $_ -like "$filter*" } |
@@ -60,7 +60,7 @@ function script:gitBranches($filter, $includeHEAD = $false) {
 }
 
 function script:gitRemoteBranches($remote, $ref, $filter) {
-    git -c color.branch=false branch -r |
+    git branch --no-color -r |
         where { $_ -like "  $remote/$filter*" } |
         foreach { $ref + ($_ -replace "  $remote/","") }
 }
