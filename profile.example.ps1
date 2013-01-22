198f2ab337593e943dddb47fbc19f5f9d699c077
Keith Dahlby <dahlbyk@gmail.com>
2013-01-22 09:18:07 -0600

    Update prompt to use $pwd.ProviderPath
    Suggested by @rpardee (#75)

--- profile.example.ps1
+++ profile.example.ps1
@@ -15,7 +15,7 @@ function prompt {
     # Reset color, which can be messed up by Enable-GitColors
     $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
 
-    Write-Host($pwd) -nonewline
+    Write-Host($pwd.ProviderPath) -nonewline
 
     Write-VcsStatus
 
