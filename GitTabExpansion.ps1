526b9b74438f0b96e1eefa80826bf71f82a282c6
Jan Baer <jan.baer@check24.de>
2013-01-24 17:04:07 +0100

    Support for "git flow" added
    Closes #76

--- GitTabExpansion.ps1
+++ GitTabExpansion.ps1
@@ -14,14 +14,26 @@ $subcommands = @{
     submodule = 'add status init update summary foreach sync'
     svn = 'init fetch clone rebase dcommit branch tag log blame find-rev set-tree create-ignore show-ignore mkdirs commit-diff info proplist propget show-externals gc reset'
     tfs = 'bootstrap checkin checkintool ct cleanup cleanup-workspaces clone diagnostics fetch help init pull quick-clone rcheckin shelve shelve-list unshelve verify'
+    flow = 'init feature release hotfix'
 }
 
-function script:gitCmdOperations($command, $filter) {
-    $subcommands.$command -split ' ' |
+$gitflowsubcommands = @{
+    feature = 'list start finish publish track diff rebase checkout pull'
+    release = 'list start finish publish track'
+    hotfix = 'list start finish publish track'
+}
+
+function script:gitCmdOperations($commands, $command, $filter) {
+    $commands.$command -split ' ' |
         where { $_ -like "$filter*" }
 }
 
+
 $script:someCommands = @('add','am','annotate','archive','bisect','blame','branch','bundle','checkout','cherry','cherry-pick','citool','clean','clone','commit','config','describe','diff','difftool','fetch','format-patch','gc','grep','gui','help','init','instaweb','log','merge','mergetool','mv','notes','prune','pull','push','rebase','reflog','remote','rerere','reset','revert','rm','shortlog','show','stash','status','submodule','svn','tag','whatchanged')
+if ((git flow 2> $null) -ne $null) {
+    $script:someCommands += 'flow'
+}
+
 
 function script:gitCommands($filter, $includeAliases) {
     $cmdList = @()
@@ -128,7 +140,13 @@ function GitTabExpansion($lastBlock) {
 
         # Handles git <cmd> <op>
         "^(?<cmd>$($subcommands.Keys -join '|'))\s+(?<op>\S*)$" {
-            gitCmdOperations $matches['cmd'] $matches['op']
+            gitCmdOperations $subcommands $matches['cmd'] $matches['op']
+        }
+
+
+        # Handles git flow <cmd> <op>
+        "^flow (?<cmd>$($gitflowsubcommands.Keys -join '|'))\s+(?<op>\S*)$" {
+            gitCmdOperations $gitflowsubcommands $matches['cmd'] $matches['op']
         }
 
         # Handles git remote (rename|rm|set-head|set-branches|set-url|show|prune) <stash>
