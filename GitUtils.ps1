22d1fc48fa78bb14ed22f024400c9e64ef8c3c34
Paul Marston <paulmarsy@users.noreply.github.com>
2014-11-09 15:43:00 +0000

    Stop Get-Process from adding exceptions to $Error
    Updating the Get-Process call in GitUtils.ps1 so exceptions aren't added to the global $Error array
    
    This occurs when pageant or ssh-agent aren't running, and therefore there is no process / pid to retrieve. However this is a valid scenario so shouldn't be added to the global $Error array.
    
    Also being more explicit with the check of the returned value from Get-Process

--- GitUtils.ps1
+++ GitUtils.ps1
@@ -244,13 +244,13 @@ function Set-TempEnv($key, $value) {
 # is a running agent.
 function Get-SshAgent() {
     if ($env:GIT_SSH -imatch 'plink') {
-        $pageantPid = Get-Process pageant -ErrorAction SilentlyContinue | Select -ExpandProperty Id
-        if ($pageantPid) { return $pageantPid }
+        $pageantPid = Get-Process pageant -ErrorAction Ignore | Select -ExpandProperty Id
+        if ($null -ne $pageantPid) { return $pageantPid }
     } else {
         $agentPid = $Env:SSH_AGENT_PID
         if ($agentPid) {
-            $sshAgentProcess = Get-Process -Id $agentPid -ErrorAction SilentlyContinue
-            if ($sshAgentProcess -and ($sshAgentProcess.Name -eq 'ssh-agent')) {
+            $sshAgentProcess = Get-Process -Id $agentPid -ErrorAction Ignore
+            if ($null -ne $sshAgentProcess -and ($sshAgentProcess.Name -eq 'ssh-agent')) {
                 return $agentPid
             } else {
                 setenv 'SSH_AGENT_PID', $null
