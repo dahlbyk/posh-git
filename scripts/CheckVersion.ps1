4e1b1b69e6b3da7358c066e3253b5a1be94b0fdf
Greg Zimmerman <thezim@gmail.com>
2015-09-08 22:09:20 -0400

    Move script file.

---
+++ scripts/CheckVersion.ps1
@@ -0,0 +1,18 @@
+$Global:GitMissing = $false
+
+if (!(Get-Command git -TotalCount 1 -ErrorAction SilentlyContinue)) {
+    Write-Warning "git command could not be found. Please create an alias or add it to your PATH."
+    $Global:GitMissing = $true
+    return
+}
+
+$requiredVersion = [Version]'1.7.2'
+if ((git --version 2> $null) -match '(?<ver>\d+(?:\.\d+)+)') {
+    $version = [Version]$Matches['ver']
+}
+if ($version -lt $requiredVersion) {
+    Write-Warning "posh-git requires Git $requiredVersion or better. You have $version."
+    $false
+} else {
+    $true
+}
