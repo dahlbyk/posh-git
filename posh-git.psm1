83caec17642f1d2b4322d13c97f5fbc3f6a66642
Keith Dahlby <dahlbyk@gmail.com>
2016-05-09 14:05:14 -0500

    Recommend PowerShell 5.0
    Per https://github.com/dahlbyk/posh-git/issues/163#issuecomment-217773942

--- posh-git.psm1
+++ posh-git.psm1
@@ -6,7 +6,7 @@ $psv = $PSVersionTable.PSVersion
 
 if ($psv.Major -lt 3 -and !$NoVersionWarn) {
     Write-Warning ("posh-git support for PowerShell 2.0 is deprecated; you have version $($psv).`n" +
-    "To download version 3.0, please visit https://www.microsoft.com/en-us/download/details.aspx?id=34595`n" +
+    "To download version 5.0, please visit https://www.microsoft.com/en-us/download/details.aspx?id=50395`n" +
     "For more information and to discuss this, please visit https://github.com/dahlbyk/posh-git/issues/163`n" +
     "To suppress this warning, change your profile to include 'Import-Module posh-git -Args `$true'.")
 }
