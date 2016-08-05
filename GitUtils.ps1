989508437f94e8c00531028caaf2308839678ef4
Joel Rowley <hjoelr@gmail.com>
2016-08-05 09:58:22 -0400

    Fixed syntax error on setenv calls.

--- GitUtils.ps1
+++ GitUtils.ps1
@@ -260,8 +260,8 @@ function Get-SshAgent() {
             if ($null -ne $sshAgentProcess) {
                 return $agentPid
             } else {
-                setenv 'SSH_AGENT_PID', $null
-                setenv 'SSH_AUTH_SOCK', $null
+                setenv 'SSH_AGENT_PID' $null
+                setenv 'SSH_AUTH_SOCK' $null
             }
         }
     }
@@ -373,8 +373,8 @@ function Stop-SshAgent() {
             Stop-Process $agentPid
         }
 
-        setenv 'SSH_AGENT_PID', $null
-        setenv 'SSH_AUTH_SOCK', $null
+        setenv 'SSH_AGENT_PID' $null
+        setenv 'SSH_AUTH_SOCK' $null
     }
 }
 
