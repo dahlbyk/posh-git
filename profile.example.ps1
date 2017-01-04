0b71116bd231983749b7c56536e8bf306016c84a
Keith Dahlby <dahlbyk@gmail.com>
2017-01-03 22:31:07 -0600

    Load only latest available version of the module

--- profile.example.ps1
+++ profile.example.ps1
@@ -1,6 +1,6 @@
 # Import the posh-git module, first via installed posh-git module.
 # If the module isn't installed, then attempt to load it from the cloned posh-git Git repo.
-$poshGitModule = Get-Module posh-git -ListAvailable
+$poshGitModule = Get-Module posh-git -ListAvailable | Sort-Object Version -Descending | Select-Object -First 1
 if ($poshGitModule) {
     $poshGitModule | Import-Module
 }
