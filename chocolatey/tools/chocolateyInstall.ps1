297e07a6969930cb686f8852674498395e0af4d3
Keith Dahlby <dahlbyk@gmail.com>
2017-01-05 13:15:20 -0600

    Remove Chocolatey prompt override

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -1,8 +1,4 @@
-﻿function Insert-Script([ref]$originalScript, $script) {
-    if(!($originalScript.Value -Contains $script)) { $originalScript.Value += $script }
-}
-
-try {
+﻿try {
     $binRoot = Get-BinRoot
 
     $oldPromptOverride = "if(Test-Path Function:\Prompt) {Rename-Item Function:\Prompt PrePoshGitPrompt -Force}"
@@ -16,7 +12,7 @@ try {
         remove-item $poshgitPath -recurse -force
       }
     } catch {
-      Write-Host 'Could not remove poshgit folder'
+      Write-Host "Could not remove `'$poshgitPath`'"
     }
 
     $poshGitInstall = if($env:poshGit -ne $null){ $env:poshGit } else {'https://github.com/dahlbyk/posh-git/zipball/master'}
@@ -30,26 +26,15 @@ try {
         $pgitExample = "$pgitDir\profile.example.ps1"
         foreach($line in $oldProfile) {
             if($line.ToLower().Contains("$poshgitPath".ToLower())) {
-                Insert-Script ([REF]$newProfile) $oldPromptOverride
                 $line = ". '$pgitExample'"
             }
             if($line.Trim().Length -gt 0) {  $newProfile += $line }
         }
-        # Save any previous Prompt logic
-        Insert-Script ([REF]$newProfile) $oldPromptOverride
         Set-Content -path $profile -value $newProfile -Force
     }
 
     $installer = Join-Path $pgitDir 'install.ps1'
     & $installer
-
-    $newProfile = @(Get-Content $PROFILE)
-    Insert-Script ([REF]$newProfile) "Rename-Item Function:\Prompt PoshGitPrompt -Force"
-
-    # function that will run previous prompt logic and then the poshgit logic
-    # all output from previous prompts will be swallowed
-    Insert-Script ([REF]$newProfile) $newPromptOverride
-    Set-Content -path $profile  -value $newProfile -Force
 } catch {
   try {
     if($oldProfile){ Set-Content -path $PROFILE -value $oldProfile -Force }
