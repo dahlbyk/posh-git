962d38b867a4c5999c046399c9cd2aac63177732
Keith Dahlby <dahlbyk@gmail.com>
2017-01-05 13:22:08 -0600

    Add Chocolatey uninstall

--- chocolatey/tools/chocolateyUninstall.ps1
+++ chocolatey/tools/chocolateyUninstall.ps1
@@ -2,17 +2,6 @@
     $binRoot = Get-BinRoot
     $poshgitPath = join-path $binRoot 'poshgit'
 
-    try {
-      if (test-path($poshgitPath)) {
-        Write-Host "Attempting to remove existing `'$poshgitPath`'."
-        remove-item $poshgitPath -recurse -force
-      }
-    } catch {
-      Write-Host "Could not remove `'$poshgitPath`'"
-    }
-
-    $poshGitInstall = if ($env:poshGit ) { $env:poshGit } else { 'https://github.com/dahlbyk/posh-git/zipball/master' }
-    Install-ChocolateyZipPackage 'poshgit' $poshGitInstall $poshgitPath
     $currentVersionPath = Get-ChildItem "$poshgitPath\*posh-git*\" | Sort-Object -Property LastWriteTime | Select-Object -Last 1
 
     if(Test-Path $PROFILE) {
@@ -24,20 +13,27 @@
         $newProfile = @()
         foreach($line in $oldProfile) {
             if ($line -like '*PoshGitPrompt*') { continue; }
+            if ($line -like '*Load posh-git example profile*') { continue; }
 
             if($line -like '. *posh-git*profile.example.ps1*') {
-                $line = ". '$currentVersionPath\profile.example.ps1'"
+                continue;
             }
             if($line -like 'Import-Module *\src\posh-git.psd1*') {
-                $line = "Import-Module '$currentVersionPath\src\posh-git.psd1'"
+                continue;
             }
             $newProfile += $line
         }
         Set-Content -path $profile -value $newProfile -Force -Encoding $oldProfileEncoding
     }
 
-    $installer = Join-Path $currentVersionPath 'install.ps1'
-    & $installer
+    try {
+      if (test-path($poshgitPath)) {
+        Write-Host "Attempting to remove existing `'$poshgitPath`'."
+        remove-item $poshgitPath -recurse -force
+      }
+    } catch {
+      Write-Host "Could not remove `'$poshgitPath`'"
+    }
 } catch {
   try {
     if($oldProfile){ Set-Content -path $PROFILE -value $oldProfile -Force -Encoding $oldProfileEncoding }
