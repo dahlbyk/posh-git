9960dd3cd7fb81ec2a7df5c0ae37476b4aab80a9
Keith Hill <r_keith_hill@hotmail.com>
2017-01-09 20:20:59 -0700

    Fix Get-SshPath to not return empty result.
    This happens when the specified $File parameter doesn't exist.  This is not desired.  Update Pester tests to reflect this.

--- test/Ssh.Tests.ps1
+++ test/Ssh.Tests.ps1
@@ -2,22 +2,26 @@
 
 Describe 'SSH Function Tests' {
     Context 'Get-SshPath Tests' {
-        It 'Returns the correct default path, given the default keyfile exists' {
-            if (!(Test-Path -LiteralPath $home\.ssh\id_rsa)) {
-                $createdFile = New-Item $Home\.ssh\id_rsa -ItemType File
-            }
+        It 'Returns the correct default path' {
             Get-SshPath | Should BeExactly (MakeNativePath $Home\.ssh\id_rsa)
-            $createdFile | Remove-Item -ErrorAction SilentlyContinue
         }
-        It 'Returns the correct path given an existing keyfile' {
-            if (!(Test-Path -LiteralPath $home\.ssh\mykey)) {
-                $createdFile = New-Item $Home\.ssh\mykey -ItemType File
-            }
+        It 'Returns the correct path given a file' {
             Get-SshPath mykey | Should BeExactly (MakeNativePath $Home\.ssh\mykey)
-            $createdFile | Remove-Item -ErrorAction SilentlyContinue
         }
-        It 'Returns nothing for a non-existin key file' {
-            Get-SshPath xyzzy | Should BeNullOrEmpty
+        It 'Returns the correct path even for a non-existin key file' {
+            $filename = 'xyzzy-eb2ff0a9-81ee-4983-b32d-530286600a51'
+            Get-SshPath $filename | Should BeExactly (MakeNativePath $Home\.ssh\$filename)
+        }
+        It 'Returns the correct path, given $Env:Home is not defined ($null)' {
+            $origEnvHome = $Env:HOME
+            try {
+                Remove-Item Env:\Home -ErrorAction SilentlyContinue
+                $Env:Home | Should BeNullOrEmpty
+                Get-SshPath | Should BeExactly (MakeNativePath $Home\.ssh\id_rsa)
+            }
+            finally {
+                Set-Item Env:\HOME -Value $origEnvHome
+            }
         }
     }
 }
