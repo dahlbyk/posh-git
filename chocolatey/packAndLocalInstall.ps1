c9c1b52fc2203851261c60f2b99c656fe8fae951
Keith Dahlby <dahlbyk@gmail.com>
2017-01-30 20:29:02 -0600

    Confirm remote tag, with -Force option to create

--- chocolatey/packAndLocalInstall.ps1
+++ chocolatey/packAndLocalInstall.ps1
@@ -1,10 +1,20 @@
+param ($Remote = 'origin', [switch]$Force)
 pushd $PSScriptRoot
 
 $nuspec = [xml](Get-Content poshgit.nuspec)
 $version = $nuspec.package.metadata.version
 
 $ErrorActionPreference = 'Stop'
-git rev-parse "v$version" 2>$null
+$remoteRef = $(git ls-remote $Remote "v$version")
+if (!$remoteRef) {
+    if ($Force) {
+        git push -f $Remote "HEAD:refs/tags/v$version"
+    }
+    else {
+        Write-Warning "'$Remote/v$version' not found! Use -Force to create tag at HEAD."
+        return
+    }
+}
 
 choco pack poshgit.nuspec
 choco install -f -y poshgit -pre --version=$version -s .
