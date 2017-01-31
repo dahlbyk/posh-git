9a1d91db0b33180266daf5af1c7cacf42fe04a82
Keith Dahlby <dahlbyk@gmail.com>
2017-01-30 19:38:22 -0600

    Specify local version to install

--- chocolatey/packAndLocalInstall.ps1
+++ chocolatey/packAndLocalInstall.ps1
@@ -1,4 +1,9 @@
 pushd $PSScriptRoot
+
+$nuspec = [xml](Get-Content poshgit.nuspec)
+$version = $nuspec.package.metadata.version
+
 choco pack poshgit.nuspec
-choco install -f -y poshgit -pre -s .
+choco install -f -y poshgit -pre --version=$version -s .
+
 popd
