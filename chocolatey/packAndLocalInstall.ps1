8161c8f02edb0d8aafe0c85f638dad7b435767f0
Keith Dahlby <dahlbyk@gmail.com>
2017-01-30 19:53:53 -0600

    Fail if the version tag doesn't exist

--- chocolatey/packAndLocalInstall.ps1
+++ chocolatey/packAndLocalInstall.ps1
@@ -3,6 +3,9 @@ pushd $PSScriptRoot
 $nuspec = [xml](Get-Content poshgit.nuspec)
 $version = $nuspec.package.metadata.version
 
+$ErrorActionPreference = 'Stop'
+git rev-parse "v$version" 2>$null
+
 choco pack poshgit.nuspec
 choco install -f -y poshgit -pre --version=$version -s .
 
