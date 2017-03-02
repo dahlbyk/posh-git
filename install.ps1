d681d82f221c7c4c52944bcb28d9e9a69beccc69
Tolga Balci <tolga.balci@gmail.com>
2017-03-02 04:24:07 -0500

    Added Verbose parameter to install.ps1

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
-param([switch]$WhatIf = $false, [switch]$Force = $false)
+param([switch]$WhatIf = $false, [switch]$Force = $false, [switch]$Verbose = $false)
 
 $installDir = Split-Path $MyInvocation.MyCommand.Path -Parent
 
 Import-Module $installDir\src\posh-git.psd1
-Add-PoshGitToProfile -WhatIf:$WhatIf -Force:$Force
+Add-PoshGitToProfile -WhatIf:$WhatIf -Force:$Force -Verbose:$Verbose
