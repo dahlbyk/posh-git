9819b076f3d616d26184d4e96da7c0308f434fd4
Keith Dahlby <dahlbyk@gmail.com>
2017-12-26 23:47:01 -0600

    Extract Test-Administrator helper

--- src/Utils.ps1
+++ src/Utils.ps1
@@ -54,6 +54,18 @@ function Invoke-Utf8ConsoleCommand([ScriptBlock]$cmd) {
     }
 }
 
+function Test-Administrator {
+    # PowerShell 5.x only runs on Windows so use .NET types to determine isAdminProcess
+    # Or if we are on v6 or higher, check the $IsWindows pre-defined variable.
+    if (($PSVersionTable.PSVersion.Major -le 5) -or $IsWindows) {
+        $currentUser = [Security.Principal.WindowsPrincipal]([Security.Principal.WindowsIdentity]::GetCurrent())
+        return $currentUser.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+    }
+
+    # Must be Linux or OSX, so use the id util. Root has userid of 0.
+    return 0 -eq (id -u)
+}
+
 <#
 .SYNOPSIS
     Configures your PowerShell profile (startup) script to import the posh-git
