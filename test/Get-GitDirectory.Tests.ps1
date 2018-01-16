129aa5dff1b89b28b06d0c99925c9f5063dbb5ae
Keith Hill <r_keith_hill@hotmail.com>
2018-01-15 21:22:33 -0700

    Attempt to fix last 2 macOS tests

--- test/Get-GitDirectory.Tests.ps1
+++ test/Get-GitDirectory.Tests.ps1
@@ -1,5 +1,15 @@
 . $PSScriptRoot\Shared.ps1
 
+function GetMacOSAdjustedTempPath($path) {
+    if (($PSVersionTable.PSVersion.Major -ge 6) -and $IsMacOS) {
+        # Mac OS's temp folder has a symlink in its path - /var is linked to /private/var
+        $path = "/private" + $path
+    }
+    else {
+        $path
+    }
+}
+
 Describe 'Get-GitDiretory Tests' {
     Context "Test normal repository" {
         BeforeAll {
@@ -40,6 +50,11 @@ Describe 'Get-GitDiretory Tests' {
 
             &$gitbin init $repoPath
             Set-Location $repoPath
+
+            # Git rid of Git warnings about configuring user for the commit we do below
+            &$gitbin config user.email "you@example.com"
+            &$gitbin config user.name "Pester User"
+
             'foo' > ./README.md
             &$gitbin add ./README.md
             # Quoting is a hack due to our use of the global:git function and how it converts args for invoke-expression
@@ -64,7 +79,8 @@ Describe 'Get-GitDiretory Tests' {
         It 'Returns the correct dir when under a worktree' {
             Set-Location $worktreePath
             $worktreeBaseName = Split-Path $worktreePath -Leaf
-            Get-GitDirectory | Should BeExactly (MakeGitPath $repoPath\.git\worktrees\$worktreeBaseName)
+            $path = GetMacOSAdjustedTempPath $repoPath
+            Get-GitDirectory | Should BeExactly (MakeGitPath $path\.git\worktrees\$worktreeBaseName)
         }
     }
 
@@ -93,7 +109,8 @@ Describe 'Get-GitDiretory Tests' {
         }
         It 'Returns correct path when under a child folder of the root of bare repo' {
             Set-Location $bareRepoPath\hooks -ErrorVariable Stop
-            MakeNativePath (Get-GitDirectory) | Should BeExactly $bareRepoPath
+            $path = GetMacOSAdjustedTempPath $bareRepoPath
+            Get-GitDirectory | Should BeExactly (MakeNativePath $path)
         }
     }
 
