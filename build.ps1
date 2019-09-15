ca1393ebd070e3cd00e3bd8fb3825c0a1c0e1575
Christoph Bergmeister <c.bergmeister@gmail.com>
2019-09-15 18:41:25 +0100

    Add build.ps1 script to create module with scripts being merged into main module

---
+++ build.ps1
@@ -0,0 +1,30 @@
+$outputFolder = (Join-Path $PSScriptRoot 'out')
+
+# Clean
+if (Test-Path $outputFolder) {
+    Remove-Item $outputFolder -Recurse
+}
+
+# Copy files to output folder
+Copy-Item -Path (Join-Path $PSScriptRoot 'src') -Destination $outputFolder -Recurse
+
+$mainModulePath = (Join-Path $outputFolder 'posh-git.psm1')
+$contentOfMainModule = Get-Content -Path $mainModulePath
+
+$PowerShellScriptsToBeMerged = Get-ChildItem -Path $outputFolder -Filter '*.ps1'
+foreach ($powerShellScriptToBeMerged in $PowerShellScriptsToBeMerged) {
+    $NameOfScriptToBeMerged = $powerShellScriptToBeMerged.Name
+    $scriptContentToBeMerged = Get-Content -Path $powerShellScriptToBeMerged
+    $replaceSearchText = ". `$PSScriptRoot\$NameOfScriptToBeMerged"
+    if ($contentOfMainModule.Contains($replaceSearchText)) {
+        $contentOfMainModule = $contentOfMainModule.Replace(". `$PSScriptRoot\$NameOfScriptToBeMerged",
+            @"
+#region
+$scriptContentToBeMerged
+#endregion
+"@)
+        Remove-Item $powerShellScriptToBeMerged
+    }
+}
+
+Set-Content -Path $mainModulePath -Value $contentOfMainModule
