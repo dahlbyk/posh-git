30c1e23fadd1fc3e75aeef2953ee0c77dad8bd6d
Constantine Chen <csc_dev@protonmail.com>
2020-06-22 01:19:32 -0700

    Added more tests to cover argument replacement.

--- test/GitProxyCommandExpansion.Tests.ps1
+++ test/GitProxyCommandExpansion.Tests.ps1
@@ -2,8 +2,8 @@ BeforeAll {
     . $PSScriptRoot\Shared.ps1
 }
 
-Describe 'CommandExpansion Tests' {
-    Context 'Proxy Subcommand TabExpansion Tests' {
+Describe 'Proxy Command Expansion Tests' {
+    Context 'Proxy Command TabExpansion Tests' {
         BeforeEach {
             if(Test-Path -Path Function:\Invoke-GitFunction) {
                 Rename-Item -Path Function:\Invoke-GitFunction -NewName Invoke-GitFunctionBackup
@@ -36,7 +36,7 @@ Describe 'CommandExpansion Tests' {
             $result = & $module Expand-GitProxyCommand 'igf '
             $result | Should -Be 'git checkout '
         }
-        It 'Expands a single line command with parameter' {
+        It 'Expands a single line command with short parameter' {
             function global:Invoke-GitFunction {
                 git checkout -b $args
             }
@@ -45,6 +45,16 @@ Describe 'CommandExpansion Tests' {
             $result | Should -Be 'git checkout -b '
             $result = & $module Expand-GitProxyCommand 'igf '
             $result | Should -Be 'git checkout -b '
+        }
+        It 'Expands a single line command with long parameter' {
+            function global:Invoke-GitFunction {
+                git checkout --detach $args
+            }
+            New-Alias -Name 'igf' -Value Invoke-GitFunction -Scope 'Global'
+            $result = & $module Expand-GitProxyCommand 'Invoke-GitFunction '
+            $result | Should -Be 'git checkout --detach '
+            $result = & $module Expand-GitProxyCommand 'igf '
+            $result | Should -Be 'git checkout --detach '
         }
         It 'Expands the first line in command' {
             function global:Invoke-GitFunction {
@@ -116,6 +126,19 @@ Describe 'CommandExpansion Tests' {
             $result = & $module Expand-GitProxyCommand 'igf '
             $result | Should -Be 'git checkout '
         }
+        It 'Expands simultaneously semicolon delimited and newline commands' {
+            function global:Invoke-GitFunction {
+                $a = 5;
+                Write-Host $null;
+                git checkout $args;
+                Write-Host $null;
+            }
+            New-Alias -Name 'igf' -Value Invoke-GitFunction -Scope 'Global'
+            $result = & $module Expand-GitProxyCommand 'Invoke-GitFunction '
+            $result | Should -Be 'git checkout '
+            $result = & $module Expand-GitProxyCommand 'igf '
+            $result | Should -Be 'git checkout '
+        }
         It 'Expands multiline command' {
             function global:Invoke-GitFunction {
                 git `
@@ -128,7 +151,7 @@ Describe 'CommandExpansion Tests' {
             $result = & $module Expand-GitProxyCommand 'igf '
             $result | Should -Be 'git checkout '
         }
-        It 'Expands multiline command with parameter' {
+        It 'Expands multiline command with short parameter' {
             function global:Invoke-GitFunction {
                 git `
                 checkout `
@@ -140,6 +163,19 @@ Describe 'CommandExpansion Tests' {
             $result | Should -Be 'git checkout -b '
             $result = & $module Expand-GitProxyCommand 'igf '
             $result | Should -Be 'git checkout -b '
+        }
+        It 'Expands multiline command with long parameter' {
+            function global:Invoke-GitFunction {
+                git `
+                checkout `
+                --detach `
+                $args
+            }
+            New-Alias -Name 'igf' -Value Invoke-GitFunction -Scope 'Global'
+            $result = & $module Expand-GitProxyCommand 'Invoke-GitFunction '
+            $result | Should -Be 'git checkout --detach '
+            $result = & $module Expand-GitProxyCommand 'igf '
+            $result | Should -Be 'git checkout --detach '
         }
         It 'Does not expand command if $args is not present' {
             function global:Invoke-GitFunction {
@@ -175,6 +211,62 @@ Describe 'CommandExpansion Tests' {
             $result | Should -Be 'Invoke-GitFunction '
             $result = & $module Expand-GitProxyCommand 'igf '
             $result | Should -Be 'igf '
+        }
+    }
+    Context 'Proxy Command Parameter Replacement Tests' {
+        BeforeEach {
+            if(Test-Path -Path Function:\Invoke-GitFunction) {
+                Rename-Item -Path Function:\Invoke-GitFunction -NewName Invoke-GitFunctionBackup
+            }
+            if(Test-Path -Path Alias:\igf) {
+                Rename-Item -Path Alias:\igf -NewName igfbackup
+            }
+        }
+        AfterEach {
+            if(Test-Path -Path Function:\Invoke-GitFunction) {
+                Remove-Item -Path Function:\Invoke-GitFunction
+            }
+            if(Test-Path -Path Function:\Invoke-GitFunctionBackup) {
+                Rename-Item Function:\Invoke-GitFunctionBackup Invoke-GitFunction
+            }
+            if(Test-Path -Path Alias:\igf) {
+                Remove-Item -Path Alias:\igf
+            }
+            if(Test-Path -Path Alias:\igfbackup) {
+                Rename-Item -Path Alias:\igfbackup -NewName igf
+            }
+        }
+        It 'Replaces parameter in $args' {
+            function global:Invoke-GitFunction { git checkout $args }
+            New-Alias -Name 'igf' -Value Invoke-GitFunction -Scope 'Global'
+            $result = & $module Expand-GitProxyCommand 'Invoke-GitFunction master'
+            $result | Should -Be 'git checkout master'
+            $result = & $module Expand-GitProxyCommand 'igf master'
+            $result | Should -Be 'git checkout master'
+        }
+        It 'Replaces short parameter in $args' {
+            function global:Invoke-GitFunction { git checkout $args }
+            New-Alias -Name 'igf' -Value Invoke-GitFunction -Scope 'Global'
+            $result = & $module Expand-GitProxyCommand 'Invoke-GitFunction -b master'
+            $result | Should -Be 'git checkout -b master'
+            $result = & $module Expand-GitProxyCommand 'igf -b master'
+            $result | Should -Be 'git checkout -b master'
+        }
+        It 'Replaces long parameter in $args' {
+            function global:Invoke-GitFunction { git checkout $args }
+            New-Alias -Name 'igf' -Value Invoke-GitFunction -Scope 'Global'
+            $result = & $module Expand-GitProxyCommand 'Invoke-GitFunction --detach master'
+            $result | Should -Be 'git checkout --detach master'
+            $result = & $module Expand-GitProxyCommand 'igf --detach master'
+            $result | Should -Be 'git checkout --detach master'
+        }
+        It 'Replaces mixed parameters in $args' {
+            function global:Invoke-GitFunction { git checkout $args }
+            New-Alias -Name 'igf' -Value Invoke-GitFunction -Scope 'Global'
+            $result = & $module Expand-GitProxyCommand 'Invoke-GitFunction -q -f -m --detach master'
+            $result | Should -Be 'git checkout -q -f -m --detach master'
+            $result = & $module Expand-GitProxyCommand 'igf -q -f -m --detach master'
+            $result | Should -Be 'git checkout -q -f -m --detach master'
         }
     }
     Context 'Proxy Subcommand TabExpansion Tests' {
