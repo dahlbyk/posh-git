ebf3386a755b1f08cb07a9a3e1eda1a72025e217
Constantine Chen <csc_dev@protonmail.com>
2020-06-22 21:12:40 -0700

    Recombined alias tests, since they were duplicates; cleaned up the setup and teardown sections for tests.  Updated the alias to use script scope instead of global scope in the tests.

--- test/GitProxyCommandExpansion.Tests.ps1
+++ test/GitProxyCommandExpansion.Tests.ps1
@@ -8,6 +8,10 @@ Describe 'Proxy Command Expansion Tests' {
             if(Test-Path -Path Function:\Invoke-GitFunction) {
                 Rename-Item -Path Function:\Invoke-GitFunction -NewName Invoke-GitFunctionBackup
             }
+            if(Test-Path -Path Alias:\igf) {
+                Rename-Item -Path Alias:\igf -NewName igfbackup
+            }
+            New-Alias -Name 'igf' -Value Invoke-GitFunction -Scope 'Script'
         }
         AfterEach {
             if(Test-Path -Path Function:\Invoke-GitFunction) {
@@ -16,6 +20,12 @@ Describe 'Proxy Command Expansion Tests' {
             if(Test-Path -Path Function:\Invoke-GitFunctionBackup) {
                 Rename-Item Function:\Invoke-GitFunctionBackup Invoke-GitFunction
             }
+            if(Test-Path -Path Alias:\igf) {
+                Remove-Item -Path Alias:\igf
+            }
+            if(Test-Path -Path Alias:\igfbackup) {
+                Rename-Item -Path Alias:\igfbackup -NewName igf
+            }
         }
         It 'Expands a single line command' {
             function script:Invoke-GitFunction {
@@ -23,6 +33,7 @@ Describe 'Proxy Command Expansion Tests' {
             }
             $result = & $module Expand-GitProxyCommand 'Invoke-GitFunction '
             $result | Should -Be 'git checkout '
+            $result | Should -Be (& $module Expand-GitProxyCommand 'igf ' )
         }
         It 'Expands a single line command with short parameter' {
             function script:Invoke-GitFunction {
@@ -30,6 +41,7 @@ Describe 'Proxy Command Expansion Tests' {
             }
             $result = & $module Expand-GitProxyCommand 'Invoke-GitFunction '
             $result | Should -Be 'git checkout -b '
+            $result | Should -Be (& $module Expand-GitProxyCommand 'igf ' )
         }
         It 'Expands a single line command with long parameter' {
             function script:Invoke-GitFunction {
@@ -37,6 +49,7 @@ Describe 'Proxy Command Expansion Tests' {
             }
             $result = & $module Expand-GitProxyCommand 'Invoke-GitFunction '
             $result | Should -Be 'git checkout --detach '
+            $result | Should -Be (& $module Expand-GitProxyCommand 'igf ' )
         }
         It 'Expands a single line with piped command suffix' {
             function script:Invoke-GitFunction {
@@ -44,6 +57,7 @@ Describe 'Proxy Command Expansion Tests' {
             }
             $result = & $module Expand-GitProxyCommand 'Invoke-GitFunction '
             $result | Should -Be 'git checkout --detach '
+            $result | Should -Be (& $module Expand-GitProxyCommand 'igf ' )
         }
         It 'Expands the first line in command' {
             function script:Invoke-GitFunction {
@@ -53,6 +67,7 @@ Describe 'Proxy Command Expansion Tests' {
             }
             $result = & $module Expand-GitProxyCommand 'Invoke-GitFunction '
             $result | Should -Be 'git checkout '
+            $result | Should -Be (& $module Expand-GitProxyCommand 'igf ' )
         }
         It 'Expands the middle line in command' {
             function script:Invoke-GitFunction {
@@ -62,6 +77,7 @@ Describe 'Proxy Command Expansion Tests' {
             }
             $result = & $module Expand-GitProxyCommand 'Invoke-GitFunction '
             $result | Should -Be 'git checkout '
+            $result | Should -Be (& $module Expand-GitProxyCommand 'igf ' )
         }
         It 'Expands the last line in command' {
             function script:Invoke-GitFunction {
@@ -71,6 +87,7 @@ Describe 'Proxy Command Expansion Tests' {
             }
             $result = & $module Expand-GitProxyCommand 'Invoke-GitFunction '
             $result | Should -Be 'git checkout '
+            $result | Should -Be (& $module Expand-GitProxyCommand 'igf ' )
         }
         It 'Expands semicolon delimited commands' {
             function script:Invoke-GitFunction {
@@ -78,6 +95,7 @@ Describe 'Proxy Command Expansion Tests' {
             }
             $result = & $module Expand-GitProxyCommand 'Invoke-GitFunction '
             $result | Should -Be 'git checkout '
+            $result | Should -Be (& $module Expand-GitProxyCommand 'igf ' )
         }
         It 'Expands mixed semicolon delimited and newline commands' {
             function script:Invoke-GitFunction {
@@ -86,6 +104,7 @@ Describe 'Proxy Command Expansion Tests' {
             }
             $result = & $module Expand-GitProxyCommand 'Invoke-GitFunction '
             $result | Should -Be 'git checkout '
+            $result | Should -Be (& $module Expand-GitProxyCommand 'igf ' )
         }
         It 'Expands mixed semicolon delimited and newline multiline commands' {
             function script:Invoke-GitFunction {
@@ -96,6 +115,7 @@ Describe 'Proxy Command Expansion Tests' {
             }
             $result = & $module Expand-GitProxyCommand 'Invoke-GitFunction '
             $result | Should -Be 'git checkout '
+            $result | Should -Be (& $module Expand-GitProxyCommand 'igf ' )
         }
         It 'Expands simultaneously semicolon delimited and newline commands' {
             function script:Invoke-GitFunction {
@@ -106,6 +126,7 @@ Describe 'Proxy Command Expansion Tests' {
             }
             $result = & $module Expand-GitProxyCommand 'Invoke-GitFunction '
             $result | Should -Be 'git checkout '
+            $result | Should -Be (& $module Expand-GitProxyCommand 'igf ' )
         }
         It 'Expands multiline command' {
             function script:Invoke-GitFunction {
@@ -115,6 +136,7 @@ Describe 'Proxy Command Expansion Tests' {
             }
             $result = & $module Expand-GitProxyCommand 'Invoke-GitFunction '
             $result | Should -Be 'git checkout '
+            $result | Should -Be (& $module Expand-GitProxyCommand 'igf ' )
         }
         It 'Expands multiline command with short parameter' {
             function script:Invoke-GitFunction {
@@ -125,6 +147,7 @@ Describe 'Proxy Command Expansion Tests' {
             }
             $result = & $module Expand-GitProxyCommand 'Invoke-GitFunction '
             $result | Should -Be 'git checkout -b '
+            $result | Should -Be (& $module Expand-GitProxyCommand 'igf ' )
         }
         It 'Expands multiline command with long parameter' {
             function script:Invoke-GitFunction {
@@ -135,20 +158,21 @@ Describe 'Proxy Command Expansion Tests' {
             }
             $result = & $module Expand-GitProxyCommand 'Invoke-GitFunction '
             $result | Should -Be 'git checkout --detach '
+            $result | Should -Be (& $module Expand-GitProxyCommand 'igf ' )
         }
         It 'Does not expand a single line with piped command prefix' {
             function script:Invoke-GitFunction {
                 "master" | git checkout --detach $args
             }
-            $result = & $module Expand-GitProxyCommand 'Invoke-GitFunction '
-            $result | Should -Be 'Invoke-GitFunction '
+            & $module Expand-GitProxyCommand 'Invoke-GitFunction ' | Should -Be 'Invoke-GitFunction '
+            & $module Expand-GitProxyCommand 'igf ' | Should -Be 'igf '
         }
         It 'Does not expand command if $args is not present' {
             function script:Invoke-GitFunction {
                 git checkout
             }
-            $result = & $module Expand-GitProxyCommand 'Invoke-GitFunction '
-            $result | Should -Be 'Invoke-GitFunction '
+            & $module Expand-GitProxyCommand 'Invoke-GitFunction ' | Should -Be 'Invoke-GitFunction '
+            & $module Expand-GitProxyCommand 'igf ' | Should -Be 'igf '
         }
         It 'Does not expand command if $args is not attached to the git command' {
             function script:Invoke-GitFunction {
@@ -156,8 +180,8 @@ Describe 'Proxy Command Expansion Tests' {
                 git checkout
                 Write-Host $args
             }
-            $result = & $module Expand-GitProxyCommand 'Invoke-GitFunction '
-            $result | Should -Be 'Invoke-GitFunction '
+            & $module Expand-GitProxyCommand 'Invoke-GitFunction ' | Should -Be 'Invoke-GitFunction '
+            & $module Expand-GitProxyCommand 'igf ' | Should -Be 'igf '
         }
         It 'Does not expand multiline command if $args is not attached to the git command' {
             function script:Invoke-GitFunction {
@@ -166,8 +190,8 @@ Describe 'Proxy Command Expansion Tests' {
                 checkout
                 Write-Host $args
             }
-            $result = & $module Expand-GitProxyCommand 'Invoke-GitFunction '
-            $result | Should -Be 'Invoke-GitFunction '
+            & $module Expand-GitProxyCommand 'Invoke-GitFunction ' | Should -Be 'Invoke-GitFunction '
+            & $module Expand-GitProxyCommand 'igf ' | Should -Be 'igf '
         }
     }
     Context 'Proxy Command Parameter Replacement Tests' {
@@ -200,169 +224,6 @@ Describe 'Proxy Command Expansion Tests' {
         It 'Replaces mixed parameters in $args' {
             $result = & $module Expand-GitProxyCommand 'Invoke-GitFunction -q -f -m --detach master'
             $result | Should -Be 'git checkout -q -f -m --detach master'
-        }
-    }
-    Context 'Alias of Proxy Command TabExpansion Tests' {
-        BeforeEach {
-            if(Test-Path -Path Function:\Invoke-GitFunction) {
-                Rename-Item -Path Function:\Invoke-GitFunction -NewName Invoke-GitFunctionBackup
-            }
-            if(Test-Path -Path Alias:\igf) {
-                Rename-Item -Path Alias:\igf -NewName igfbackup
-            }
-            New-Alias -Name 'igf' -Value Invoke-GitFunction -Scope 'Global'
-        }
-        AfterEach {
-            if(Test-Path -Path Function:\Invoke-GitFunction) {
-                Remove-Item -Path Function:\Invoke-GitFunction
-            }
-            if(Test-Path -Path Function:\Invoke-GitFunctionBackup) {
-                Rename-Item Function:\Invoke-GitFunctionBackup Invoke-GitFunction
-            }
-            if(Test-Path -Path Alias:\igf) {
-                Remove-Item -Path Alias:\igf
-            }
-            if(Test-Path -Path Alias:\igfbackup) {
-                Rename-Item -Path Alias:\igfbackup -NewName igf
-            }
-        }
-        It 'Expands a single line command' {
-            function script:Invoke-GitFunction {
-                git checkout $args
-            }
-            & $module Expand-GitProxyCommand 'Invoke-GitFunction ' | Should -Be (& $module Expand-GitProxyCommand 'igf ' )
-        }
-        It 'Expands a single line command with short parameter' {
-            function script:Invoke-GitFunction {
-                git checkout -b $args
-            }
-            & $module Expand-GitProxyCommand 'Invoke-GitFunction ' | Should -Be (& $module Expand-GitProxyCommand 'igf ')
-        }
-        It 'Expands a single line command with long parameter' {
-            function script:Invoke-GitFunction {
-                git checkout --detach $args
-            }
-            & $module Expand-GitProxyCommand 'Invoke-GitFunction ' | Should -Be (& $module Expand-GitProxyCommand 'igf ')
-        }
-        It 'Expands a single line with piped command suffix' {
-            function script:Invoke-GitFunction {
-                git checkout --detach $args | Write-Host
-            }
-            & $module Expand-GitProxyCommand 'Invoke-GitFunction ' | Should -Be (& $module Expand-GitProxyCommand 'igf ')
-        }
-        It 'Expands the first line in command' {
-            function script:Invoke-GitFunction {
-                git checkout $args
-                $a = 5
-                Write-Host $null
-            }
-            & $module Expand-GitProxyCommand 'Invoke-GitFunction ' | Should -Be (& $module Expand-GitProxyCommand 'igf ')
-        }
-        It 'Expands the middle line in command' {
-            function script:Invoke-GitFunction {
-                $a = 5
-                git checkout $args
-                Write-Host $null
-            }
-            & $module Expand-GitProxyCommand 'Invoke-GitFunction ' | Should -Be (& $module Expand-GitProxyCommand 'igf ')
-        }
-        It 'Expands the last line in command' {
-            function script:Invoke-GitFunction {
-                $a = 5
-                Write-Host $null
-                git checkout $args
-            }
-            & $module Expand-GitProxyCommand 'Invoke-GitFunction ' | Should -Be (& $module Expand-GitProxyCommand 'igf ')
-        }
-        It 'Expands semicolon delimited commands' {
-            function script:Invoke-GitFunction {
-                $a = 5; git checkout $args; Write-Host $null;
-            }
-            & $module Expand-GitProxyCommand 'Invoke-GitFunction ' | Should -Be (& $module Expand-GitProxyCommand 'igf ')
-        }
-        It 'Expands mixed semicolon delimited and newline commands' {
-            function script:Invoke-GitFunction {
-                $a = 5; Write-Host $null
-                git checkout $args; Write-Host $null;
-            }
-            & $module Expand-GitProxyCommand 'Invoke-GitFunction ' | Should -Be (& $module Expand-GitProxyCommand 'igf ')
-        }
-        It 'Expands mixed semicolon delimited and newline multiline commands' {
-            function script:Invoke-GitFunction {
-                $a = 5; Write-Host $null
-                git `
-                checkout `
-                $args; Write-Host $null;
-            }
-            & $module Expand-GitProxyCommand 'Invoke-GitFunction ' | Should -Be (& $module Expand-GitProxyCommand 'igf ')
-        }
-        It 'Expands simultaneously semicolon delimited and newline commands' {
-            function script:Invoke-GitFunction {
-                $a = 5;
-                Write-Host $null;
-                git checkout $args;
-                Write-Host $null;
-            }
-            & $module Expand-GitProxyCommand 'Invoke-GitFunction ' | Should -Be (& $module Expand-GitProxyCommand 'igf ')
-        }
-        It 'Expands multiline command' {
-            function script:Invoke-GitFunction {
-                git `
-                checkout `
-                $args
-            }
-            & $module Expand-GitProxyCommand 'Invoke-GitFunction ' | Should -Be (& $module Expand-GitProxyCommand 'igf ')
-        }
-        It 'Expands multiline command with short parameter' {
-            function script:Invoke-GitFunction {
-                git `
-                checkout `
-                -b `
-                $args
-            }
-            & $module Expand-GitProxyCommand 'Invoke-GitFunction ' | Should -Be (& $module Expand-GitProxyCommand 'igf ')
-        }
-        It 'Expands multiline command with long parameter' {
-            function script:Invoke-GitFunction {
-                git `
-                checkout `
-                --detach `
-                $args
-            }
-            & $module Expand-GitProxyCommand 'Invoke-GitFunction ' | Should -Be (& $module Expand-GitProxyCommand 'igf ')
-        }
-        It 'Does not expand command if $args is not present' {
-            function script:Invoke-GitFunction {
-                git checkout
-            }
-            $result = & $module Expand-GitProxyCommand 'igf '
-            $result | Should -Be 'igf '
-        }
-        It 'Does not expand a single line with piped command prefix' {
-            function script:Invoke-GitFunction {
-                "master" | git checkout --detach $args
-            }
-            $result = & $module Expand-GitProxyCommand 'igf '
-            $result | Should -Be 'igf '
-        }
-        It 'Does not expand command if $args is not attached to the git command' {
-            function script:Invoke-GitFunction {
-                $a = 5
-                git checkout
-                Write-Host $args
-            }
-            $result = & $module Expand-GitProxyCommand 'igf '
-            $result | Should -Be 'igf '
-        }
-        It 'Does not expand multiline command if $args is not attached to the git command' {
-            function script:Invoke-GitFunction {
-                $a = 5
-                git `
-                checkout
-                Write-Host $args
-            }
-            $result = & $module Expand-GitProxyCommand 'igf '
-            $result | Should -Be 'igf '
         }
     }
     Context 'Proxy Subcommand TabExpansion Tests' {
