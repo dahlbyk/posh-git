68a96b96e22bdbcdc98e5b12705d3cbc39f14132
Keith Hill <r_keith_hill@hotmail.com>
2021-01-30 12:56:01 -0700

    Style cleanup of the choco install script

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -2,21 +2,22 @@
     $poshgitPath = join-path (Get-ToolsLocation) 'poshgit'
 
     try {
-      if (test-path($poshgitPath)) {
-        Write-Host "Attempting to remove existing `'$poshgitPath`'."
-        remove-item $poshgitPath -recurse -force
-      }
-    } catch {
-      Write-Host "Could not remove `'$poshgitPath`'"
+        if (test-path($poshgitPath)) {
+            Write-Host "Attempting to remove existing `'$poshgitPath`'."
+            remove-item $poshgitPath -recurse -force
+        }
+    }
+    catch {
+        Write-Host "Could not remove `'$poshgitPath`'"
     }
 
     $version = "v$Env:chocolateyPackageVersion"
     if ($version -eq 'v') { $version = 'master' }
-    $poshGitInstall = if ($env:poshGit ) { $env:poshGit } else { "https://github.com/dahlbyk/posh-git/zipball/$version" }
+    $poshGitInstall = if ($env:poshGit) { $env:poshGit } else { "https://github.com/dahlbyk/posh-git/zipball/$version" }
     $zip = Install-ChocolateyZipPackage 'poshgit' $poshGitInstall $poshgitPath
     $currentVersionPath = Get-ChildItem "$poshgitPath\*posh-git*\" | Sort-Object -Property LastWriteTime | Select-Object -Last 1
 
-    if(Test-Path $PROFILE) {
+    if (Test-Path $PROFILE) {
         $oldProfile = @(Get-Content $PROFILE)
 
         . $currentVersionPath\src\Utils.ps1
@@ -24,26 +25,29 @@
 
         $newProfile = @()
         foreach($line in $oldProfile) {
-            if ($line -like '*PoshGitPrompt*') { continue; }
+            if ($line -like '*PoshGitPrompt*') { continue }
 
-            if($line -like '. *posh-git*profile.example.ps1*') {
+            if ($line -like '. *posh-git*profile.example.ps1*') {
                 $line = ". '$currentVersionPath\profile.example.ps1' choco"
             }
-            if($line -like 'Import-Module *\src\posh-git.psd1*') {
+
+            if ($line -like 'Import-Module *\src\posh-git.psd1*') {
                 $line = "Import-Module '$currentVersionPath\src\posh-git.psd1'"
             }
             $newProfile += $line
         }
+
         Set-Content -path $profile -value $newProfile -Force -Encoding $oldProfileEncoding
     }
 
     $installer = Join-Path $currentVersionPath 'install.ps1'
     & $installer
 } catch {
-  try {
-    if($oldProfile){ Set-Content -path $PROFILE -value $oldProfile -Force -Encoding $oldProfileEncoding }
-  }
-  catch {}
-  throw
+    try {
+        if ($oldProfile ) {
+            Set-Content -path $PROFILE -value $oldProfile -Force -Encoding $oldProfileEncoding
+        }
+    }
+    catch {}
+    throw
 }
-
